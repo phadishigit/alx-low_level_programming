3e1fce1c178ddff71fd6e3aa2dd3a6b892d9cad8
phadishigit <ruphusruphy80@gmail.com>
2022-12-09 01:22:06 -0800

    4-puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
- * /
- 
- int main(void)
- {
- puts("\"Progremming is like building a multilingual puzzle");
- return (0);
-
- }
+ * main - main block
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
