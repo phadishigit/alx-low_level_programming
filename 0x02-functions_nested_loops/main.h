532b39dfd88609d3e47c4f5486818f382a19d719
phadishigit <ruphusruphy80@gmail.com>
2022-12-14 06:57:02 -0800

    task putchar 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-vint add(int, int);
+int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 
