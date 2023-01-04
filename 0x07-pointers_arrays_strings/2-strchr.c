9a7e3af05e6ac27bfc9b28ba51b17ecb5d21595a
phadishigit <ruphusruphy80@gmail.com>
2023-01-04 04:54:21 -0800

    task 2 strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,23 +5,16 @@
  * @s: String
  * @c: character to search
  * Return: Pointer to the first occurence of the character c in the string s
- **/
+ */
 
 char *_strchr(char *s, char c);
 {
-	while (*s != '\0') /*Declaring WHILE*/
-	{
-		if (*s == c) /*if s == c*/
-		{
-			return (s); /*return s*/
-		}
-		++s;
-	}
-	if (*s == c)
-	{
-		return (s);
-	}
+	unsigned int i = 0;
 
-	return (0);/* values null*/
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
-
